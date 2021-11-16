837fcf85c9d9807764d6eca1d408bacbef112ea2
otherJL0 <jonathanglopez@gmail.com>
2021-11-16 12:14:24 -0500

    Adding simple repl and ts parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use rustyline::{Editor, Result};
+use tree_sitter::{Language, Parser};
+
+fn main() -> Result<()> {
+    let mut parser = Parser::new();
+    extern "C" {
+        fn tree_sitter_sqlite() -> Language;
+    }
+    let language = unsafe { tree_sitter_sqlite() };
+    parser.set_language(language).unwrap();
+    let mut rl = Editor::<()>::new();
+    loop {
+        let line = rl.readline("xlite> ")?;
+        println!("Line: {}", line); // eval / print
+    }
 }
